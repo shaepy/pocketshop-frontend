9ad046b2127ef9211580aab09f480fb89a5cd8cf
Bronts <bronteda@gmail.com>
2025-10-19 11:32:42 +0100

    Set-up react frontend and setup a test to our backend connection

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+import TestRunShops from "../services/TestRun";
+import "./App.css";
+
+function App() {
+  const [shops, setShops] = useState([]);
+  useEffect(() => {
+    const fetchShops = async () => {
+      const shops = await TestRunShops();
+      console.log("shops",shops)
+      setShops(shops);
+    };
+
+    fetchShops();
+  }, []);
+
+  return (
+    <>
+      <h1>Welcome To PocketShop</h1>
+      <ul>
+        {shops.map((shop) => (
+          <li key={shop.id}>{shop.name}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
