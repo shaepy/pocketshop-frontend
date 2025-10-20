f95d015e1b4f21c2d1fbfeeb0ed0d7bdb32b3667
shaexintran <shaexin@icloud.com>
2025-10-20 10:53:56 -0700

    UserContext created

--- src/App.jsx
+++ src/App.jsx
@@ -3,24 +3,24 @@ import TestRunShops from "../services/TestRun";
 import "./App.css";
 
 function App() {
-  const [shops, setShops] = useState([]);
-  useEffect(() => {
-    const fetchShops = async () => {
-      const shops = await TestRunShops();
-      console.log("shops",shops)
-      setShops(shops);
-    };
+  // const [shops, setShops] = useState([]);
+  // useEffect(() => {
+  //   const fetchShops = async () => {
+  //     const shops = await TestRunShops();
+  //     console.log("shops",shops)
+  //     setShops(shops);
+  //   };
 
-    fetchShops();
-  }, []);
+  //   fetchShops();
+  // }, []);
 
   return (
     <>
       <h1>Welcome To PocketShop</h1>
       <ul>
-        {shops.map((shop) => (
+        {/* {shops.map((shop) => (
           <li key={shop.id}>{shop.name}</li>
-        ))}
+        ))} */}
       </ul>
     </>
   );
